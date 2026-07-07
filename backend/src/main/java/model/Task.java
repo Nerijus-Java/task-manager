639dfdb06f51fe07160436c38a1b46b53723e0ee
Nerijus Viliusis <pcneriyus3@gmail.com>
2026-07-07 18:35:47 +0300

    Added Task model

---
+++ backend/src/main/java/model/Task.java
@@ -0,0 +1,22 @@
+package model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "tasks")
+@Getter @Setter
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String description;
+    private boolean completed;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
