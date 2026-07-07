2444d19228bc566de40bec971fef0225d5fd3fb2
Nerijus Viliusis <pcneriyus3@gmail.com>
2026-07-07 18:21:07 +0300

    Added User Model

---
+++ backend/src/main/java/model/User.java
@@ -0,0 +1,20 @@
+package model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter @Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+
+}
