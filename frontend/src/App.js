e8459a0f37aed65d9444689f99242bd1ace712d7
Nerijus Viliusis <pcneriyus3@gmail.com>
2026-07-13 15:47:21 +0300

    Frontend: Added react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import RegistrationPage from './pages/RegistrationPage';
+
 
 function App() {
   return (
     <div >
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/register" element={<RegistrationPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
