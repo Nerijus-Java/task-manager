0217ebe8170c8a9c8b902ca640f9973527477111
Nerijus Viliusis <pcneriyus3@gmail.com>
2026-07-13 20:30:36 +0300

    Frontend: Added theme

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,38 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import RegistrationPage from './pages/RegistrationPage';
 import NavBar from './components/NavBar';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+import RegistrationPage from './pages/RegistrationPage';
+import HomePage from './pages/HomePage';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#1b2127',
+    },
+    secondary: {
+      main: '#2d56c9', 
+    },
+    background: {
+      default: '#4d4d4d', 
+    },
+  },
+});
 
 function App() {
   return (
+    <ThemeProvider theme={theme}>
       <BrowserRouter>
+        <CssBaseline />
         <NavBar />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/register" element={<RegistrationPage />} />
         </Routes>
       </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
