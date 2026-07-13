cc35a7c8d28523ae55b486017c5c8c06151dae07
Nerijus Viliusis <pcneriyus3@gmail.com>
2026-07-13 15:33:46 +0300

    Frontend: connected Front end to Backend with axios

---
+++ frontend/src/services/ApiService.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const API_BASE_URL = "http://localhost:8080/api";
+
+
+//Tasks
+export const getTasks = (userId) => {
+  return axios.get(`${API_BASE_URL}/tasks/user/${userId}`);
+};
+
+export const createTask = (taskData, userId) => {
+  return axios.post(`${API_BASE_URL}/tasks/user/${userId}`, taskData);
+};
+
+//Users
+export const registerUser = (userData) => {
+  return axios.post(`${API_BASE_URL}/users/register`, userData);
+};
+
