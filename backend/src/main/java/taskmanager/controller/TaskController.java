0ca7cd892426b0984ed2729c3fb6dc6350e47615
Nerijus Viliusis <pcneriyus3@gmail.com>
2026-07-21 14:08:19 +0300

    Backend: Added createTask, updateTaskStatus,deleteTask

--- backend/src/main/java/taskmanager/controller/TaskController.java
+++ backend/src/main/java/taskmanager/controller/TaskController.java
@@ -1,6 +1,7 @@
 package taskmanager.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import taskmanager.model.Task;
 import taskmanager.model.User;
 import org.springframework.web.bind.annotation.*;
@@ -8,6 +9,7 @@ import taskmanager.repository.TaskRepository;
 import taskmanager.repository.UserRepository;
 
 import java.util.List;
+import java.util.Map;
 
 
 @RestController
@@ -23,12 +25,43 @@ public class TaskController {
         return taskRepository.findByUserId(userId);
     }
 
-    @PostMapping("/user/{userId}")
-    public Task createTask(@PathVariable long userId, @RequestBody Task task) {
+    @PostMapping("/assign/{workerId}")
+    public ResponseEntity<?> createTask(@PathVariable long workerId, @RequestBody Task task) {
 
-        User user = userRepository.findById(userId).orElseThrow();
+        User user = userRepository.findById(workerId).orElseThrow(() -> new RuntimeException("Worker not found"));
         task.setUser(user);
 
-        return taskRepository.save(task);
+        Task savedTask = taskRepository.save(task);
+
+        return ResponseEntity.ok(savedTask);
+    }
+
+    @PatchMapping("/{taskId/status}")
+    public ResponseEntity<?> updateTaskStatus(@PathVariable long taskId, @RequestBody Map<String, String> request) {
+
+        Task task = taskRepository.findById(taskId)
+                .orElseThrow(() -> new RuntimeException("Task not found"));
+
+        String status = request.get("status");
+
+        try {
+            task.setStatus(taskmanager.model.TaskStatus.valueOf(status.toUpperCase()));
+        } catch (IllegalArgumentException | NullPointerException e) {
+            return ResponseEntity.badRequest().body("Invalid task status!");
+        }
+
+        Task updatedTask = taskRepository.save(task);
+        return ResponseEntity.ok(updatedTask);
+    }
+
+    @DeleteMapping("/{taskID}")
+    public ResponseEntity<?> deleteTask(@PathVariable long taskID) {
+        if (!taskRepository.existsById(taskID)){
+            return ResponseEntity.badRequest().body("Cannot delete: Task not found");
+        }
+
+        taskRepository.deleteById(taskID);
+
+        return ResponseEntity.ok("Task deleted");
     }
 }
